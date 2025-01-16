ab5a5c696ab8c86fa235c8b531d119b343adc38d
Jacques-Mock-Schindler <jacques.mock@kbw.ch>
2025-01-16 16:36:48 +0100

    Lege die Klasse BNode an
    Die Klasse dient der Anlage eines Nodes für einen
    BST.

--- docs/250115/sequenzen/nodes.py
+++ docs/250115/sequenzen/nodes.py
@@ -10,3 +10,13 @@ class Node:
         
     def set_next(self, next):
         self.next = next
+        
+class BNode:
+    def __init__(self, value):
+        self.value = value
+        self.parent = None
+        self.left = None
+        self.right = None
+        
+    def __str__(self):
+        return f'{super().__str__()}\nParent: {self.parent}\nValue: {self.value}\nLinks: {self.left}\tRechts: {self.right}'
