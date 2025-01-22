c15435a63b809838ec8d6e44c8085953c635ef26
Jacques-Mock-Schindler <jacques.mock@kbw.ch>
2025-01-22 08:21:34 +0100

    reduziere das node file
    Das Modul nodes.py beinhaltet nur noch den Basisknoten.

--- docs/250122/src/nodes.py
+++ docs/250122/src/nodes.py
@@ -10,19 +10,3 @@ class Node:
         
     def set_next(self, next):
         self.next = next
-        
-class BNode:
-    def __init__(self, value):
-        self.value = value
-        self.parent = None
-        self.left = None
-        self.right = None
-        
-    def __str__(self):
-        return (
-            f'\tParent: {self.parent if self.parent is None else self.parent.value}\n'
-            f'\tValue: {self.value}\n'
-            f'Left: {self.left if self.left is None else self.left.value}\t'
-            f'Right: {self.right if self.right is None else self.right.value}'
-        )
-        
