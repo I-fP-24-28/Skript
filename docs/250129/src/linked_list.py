b02bdc93784a1e43684a2bd5de945f9173a6acc5
Jacques-Mock-Schindler <jacques.mock@kbw.ch>
2025-01-23 08:09:38 +0100

    Implementiere Queue

--- docs/250129/src/linked_list.py
+++ docs/250129/src/linked_list.py
@@ -7,31 +7,31 @@ class LinkedList:
         self.length = 0
         if value:
             node = Node(value)
-            self.head = node
+            self.tail = node
             self.length += 1
         else:
-            self.head = None
+            self.tail = None
             
     def append(self, value):
         node = Node(value)
-        node.next = self.head
-        self.head = node
+        node.next = self.tail
+        self.tail = node
         self.length += 1
         
     push = append
     
     def pop(self) -> Node:
-        result = self.head
-        self.head = result.next
+        result = self.tail
+        self.tail = result.next
         result.next = None
         self.length -= 1
         return result
         
     def search(self, value):
-        if self.head == None:
+        if self.tail == None:
             return None
         
-        node = self.head
+        node = self.tail
         
         if node.value == value:
             return node
@@ -44,15 +44,15 @@ class LinkedList:
         return None
     
     def delete(self, value):
-        if self.head == None:
+        if self.tail == None:
             return -1
         elif self.search(value) == None:
             return -1
         
         else:
-            node = self.head
+            node = self.tail
             if node.value == value:
-                self.head = node.next
+                self.tail = node.next
                 self.length -= 1
                 return 0
             
@@ -68,7 +68,34 @@ class LinkedList:
             return -1
                 
     def itrate(self):
-        node = self.head
+        node = self.tail
         while node:
             yield node.value
             node = node.next
+            
+class Queue(LinkedList):
+    def __init__(self, value=None):
+        super().__init__(value)
+        if value:
+            node = QNode(value)
+            self.tail = node
+            self.head = node
+            
+        else:
+            self.head = None
+            self.tail = None
+            
+    def append(self, value):
+        node = QNode(value)
+        prev_node = self.tail
+        self.tail = node
+        node.next = prev_node
+        prev_node.prev = self.tail
+        
+    def pop(self):
+        node = self.head
+        self.head = node.prev
+        node.prev = None
+        node.next = None
+        return node
+        
